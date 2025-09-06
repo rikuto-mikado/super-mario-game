12873b0a9a158551a5a3dffa702484b11f6c9a75
Rikuto <mikadorikuto@gmail.com>
2025-09-06 10:25:52 +0900

    start new project

---
+++ settings.py
@@ -0,0 +1,16 @@
+# Game settings
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+FPS = 60
+
+# Color settings
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+
+# Player settings
+PLAYER_SPEED = 5
+JUMP_STRENGTH = -15
+GRAVITY = 0.8
